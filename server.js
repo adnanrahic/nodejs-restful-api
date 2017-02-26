923e3d5683244b299178748239b0a0cd12b274d6
adnanrahic <ado.raha198@gmail.com>
2017-02-26 22:36:47 +0100

    misc bugfixes

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var app = require('./app');
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function() {
-  console.info('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + port);
 });
--- user/UserController.js
+++ user/UserController.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+
 router.use(bodyParser.urlencoded({ extended: true }));
 var User = require('./User');
 
@@ -12,7 +13,7 @@ router.post('/', function (req, res) {
             password : req.body.password
         }, 
         function (err, user) {
-            if (err) res.status(500).send("There was a problem adding the information to the database.");
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
             return res.status(200).send(user);
         });
 
