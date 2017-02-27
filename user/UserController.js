d2736eab5395f70c70c2db1f3282429c66f8d519
adnanrahic <ado.raha198@gmail.com>
2017-02-28 00:11:08 +0100

    fix

--- user/UserController.js
+++ user/UserController.js
@@ -14,7 +14,7 @@ router.post('/', function (req, res) {
         }, 
         function (err, user) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            return res.status(200).send(user);
+            res.status(200).send(user);
         });
 
 });
@@ -23,7 +23,7 @@ router.get('/', function (req, res) {
 
     User.find({}, function (err, users) {
         if (err) return res.status(404).send("No users found.");
-        return res.status(200).send(users);
+        res.status(200).send(users);
     });
 
 });
@@ -32,7 +32,7 @@ router.get('/:id', function (req, res) {
 
     User.findById(req.params.id, function (err, user) {
         if (err) return res.status(404).send("No user found.");
-        return res.status(200).send(user);
+        res.status(200).send(user);
     });
 
 });
@@ -43,7 +43,7 @@ router.put('/:id', function (req, res) {
         if (err) return res.status(500).send("There was a problem updating the user.");
 
         User.findById(user.id, function (err, user) {
-            return res.status(200).send(user);
+            res.status(200).send(user);
         });
     });
 
@@ -53,7 +53,7 @@ router.delete('/:id', function (req, res) {
 
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
-        return res.status(200).send("User: "+user.name+" was deleted.");
+        res.status(200).send("User: "+user.name+" was deleted.");
     });
 
 });
