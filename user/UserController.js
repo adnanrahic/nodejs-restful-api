3e9167e81fa9a3b6068f2346034d85ae2901758f
adnanrahic <ado.raha198@gmail.com>
2018-01-25 17:42:10 +0100

    added body parser for json

--- user/UserController.js
+++ user/UserController.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 var User = require('./User');
 
